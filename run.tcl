6886e8ef02ceb9e290b9421c338480793d1e958c
Dimitrios Andronikou <dimitriosa@cn92.it.auth.gr>
2022-11-03 18:20:21 +0200

    First commit

---
+++ run.tcl
@@ -0,0 +1,11 @@
+set_db init_lib_search_path /mnt/apps/prebuilt/eda/designkits/GPDK/gsclib045/lan/flow/t1u1/reference_libs/GPDK045/gsclib045_svt_v4.4/gsclib045
+
+set_db script_search_path /mnt/scratch_b/users/d/dimitriosa/Desktop/asic/prj/scripts
+
+set_db init_hdl_search_path /mnt/scratch_b/users/d/dimitriosa/Desktop/asic/prj/hdl
+
+set_db library /mnt/apps/prebuilt/eda/designkits/GPDK/gsclib045/lan/flow/t1u1/reference_libs/GPDK045/gsclib045_svt_v4.4/gsclib045/timing/fast_vdd1v0_basicCells.lib
+
+set_db lef_library {/mnt/apps/prebuilt/eda/designkits/GPDK/gsclib045/lan/flow/t1u1/reference_libs/GPDK045/gsclib045_svt_v4.4/gsclib045/lef/gsclib045_tech.lef /mnt/apps/prebuilt/eda/designkits/GPDK/gsclib045/lan/flow/t1u1/reference_libs/GPDK045/gsclib045_svt_v4.4/gsclib045/lef/gsclib045_macro.lef}
+
+read_qrc /mnt/apps/prebuilt/eda/designkits/GPDK/gsclib045/lan/flow/t1u1/reference_libs/GPDK045/gsclib045_svt_v4.4/gsclib045/qrc/qx/gpdk045.tch
